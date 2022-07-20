476fb3a01e7b3a637d5d1fa5621c36d3af076782
Yaarit <sternyaarit@gmail.com>
2022-07-20 21:36:49 +0300

    finished project

--- script.js
+++ script.js
@@ -10,13 +10,20 @@ const password = document.getElementById('Password');
 
 
 
+
 // Creating a function that adds the error class
 function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
     small.innerText = message;
-    
+};
+
+
+// Creating a function that adds the success class
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 };
 
 
@@ -25,7 +32,7 @@ function showError(input, message) {
         const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     
         return re.test(String(email).toLowerCase());
-      };
+      }
 
 
 
@@ -58,15 +65,48 @@ function checkRequiredFields(inputArr) {
     inputArr.forEach(function(input) {
         if(input.value.trim() === '') {
             showError(input, `${input.id} cannot be empty`)
-        } else if (!validEmail(email.value)) {
-            showError(email, 'Looks like this is not an email')
+        } else {
+            showSuccess(input);
         };
     }) 
 };
 
+// another function just to check if the email is valid
+function checkEmailField (input) {
+    if (email.value === '') {
+        showError(email, 'Email cannot be empty') 
+    } else if (!validEmail(email.value)) {
+        showError(email, 'Looks like this is not an email')
+    } else {
+        showSuccess(input);
+    };
+};
+
 // It's better if we write one function that goes over every field in an array instead of writing 'if' statement fot each field
+// form.addEventListener('submit', function(e) {
+//     e.preventDefault();
+// });
+
+
+
+//Creating the function
+// const validation = function(e) {
+//     e.preventDefault();
+// checkRequiredFields([firstName, lastName, email, password]);
+// checkEmailField(email);
+// };
+
+
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-checkRequiredFields([firstName, lastName, email, password]);
+    checkRequiredFields([firstName, lastName, email, password]);
+    checkEmailField(email);
 });
+
+form.addEventListener('keyup', function(e) {
+    e.preventDefault();
+    checkRequiredFields([firstName, lastName, email, password]);
+    checkEmailField(email);
+});
+
